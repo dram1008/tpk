98a44bc007f3d79b9c2b8d2ed56944579b5b6231
dram1008 <dram1008@yandex.ru>
2015-11-05 23:10:11 +0300

    init

--- views/site/search.php
+++ views/site/search.php
@@ -16,20 +16,37 @@ $this->title = 'ТПК';
 
         <?php foreach ($list as $item) { ?>
             <?php $article = new \app\models\Article($item) ?>
-            <div class="post format-image box">
-                <div class="frame">
-                    <a href="<?= $article->getLink() ?>">
-                        <img src="<?= $item['image'] ?>" alt=""/>
-                    </a>
-                </div>
-                <h2 class="title"><a href="<?= $article->getLink() ?>"><?= $item['header'] ?></a></h2>
+            <?php if ($article->getField('video')) { ?>
+                <div class="post format-video box">
+                    <div class="video frame">
+                        <iframe width="100%" height="281" src="<?= $article->getField('video') ?>"
+                                frameborder="0" allowfullscreen></iframe>
+                    </div>
+                    <h2 class="title"><a href="<?= $article->getLink() ?>"><?= $item['header'] ?></a></h2>
 
-                <p><?= $item['description'] ?></p>
+                    <p><?= $item['description'] ?></p>
 
-                <div class="details">
-                    <span class="icon-image"><a href="#"><?= Yii::$app->formatter->asDate($item['date_insert']) ?></a></span>
+                    <div class="details">
+                        <span class="icon-image"><a href="#"><?= Yii::$app->formatter->asDate($item['date_insert']) ?></a></span>
+                    </div>
+
                 </div>
-            </div>
+            <?php } else { ?>
+                <div class="post format-image box">
+                    <div class="frame">
+                        <a href="<?= $article->getLink() ?>">
+                            <img src="<?= $item['image'] ?>" alt=""/>
+                        </a>
+                    </div>
+                    <h2 class="title"><a href="<?= $article->getLink() ?>"><?= $item['header'] ?></a></h2>
+
+                    <p><?= $item['description'] ?></p>
+
+                    <div class="details">
+                        <span class="icon-image"><a href="#"><?= Yii::$app->formatter->asDate($item['date_insert']) ?></a></span>
+                    </div>
+                </div>
+            <?php } ?>
         <?php } ?>
 
     </div>
