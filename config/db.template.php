14ab7e4740024e4d235509890e060d3900a4a670
dram1008 <dram1008@yandex.ru>
2015-11-23 22:42:46 +0300

    init

---
+++ config/db.template.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class'    => 'yii\db\Connection',
+    'dsn'      => 'mysql:host=localhost;dbname=galaxysss_5',
+    'username' => 'galaxysss_5',
+    'password' => 'N9g3Y9t0',
+    'charset'  => 'utf8',
+];
